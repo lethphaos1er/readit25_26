bfbcfa2a33d7d9bb9008dd309114cecc520a4ba6
Cedric Catoul <catoul.cedric@protonmail.com>
2025-08-26 11:14:04 +0200

    slugify route défaut

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # readit25_26
+
+route posts.show
+URL: /post/id/titre-du*post
+PATTERN: ?posts=show&id=x
--- core/helpers.php
+++ core/helpers.php
@@ -12,3 +12,30 @@ function truncate(string $text, int $length = 100): string
         return $text;
     }
 }
+
+function slugify(string $text): string
+{
+    // Replace non letter or digits by -
+    $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+
+    // Transliterate
+    $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+    // Remove unwanted characters
+    $text = preg_replace('~[^-\w]+~', '', $text);
+
+    // Trim
+    $text = trim($text, '-');
+
+    // Remove duplicate -
+    $text = preg_replace('~-+~', '-', $text);
+
+    // Lowercase
+    $text = strtolower($text);
+
+    if (empty($text)) {
+        return 'n-a';
+    }
+
+    return $text;
+}
